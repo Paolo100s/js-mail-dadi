4be47d16eb463073fde3d6cabc02c9955aca81d3
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-04 14:43:37 +0200

    add event listener

---
+++ Dadi/js/myscript.js
@@ -0,0 +1,5 @@
+// DADI
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
---
+++ Mail/js/myscript.js
@@ -0,0 +1,17 @@
+// MAIL
+
+// Mail
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+// QUI ricordatevi che non è concesso usare altro oltre quello che abbiam visto oggi
+
+// variabili globali
+const mailConsentite = ["saraz@gmail.com", "franc22@gmail.com", "al-berto@gmail.com", "jhonny@gmail.com"];
+const buttonCheck = document.getElementById("check");
+
+// evento al button
+buttonCheck.addEventListener('click', checkFunction)
+
+// funzione al check
+function checkFunction() {
+    
+}
