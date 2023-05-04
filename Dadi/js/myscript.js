3c2832a744759ba179b109161d9c6e345fbb8f90
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-04 17:16:16 +0200

    add number generation

--- Dadi/js/myscript.js
+++ Dadi/js/myscript.js
@@ -3,3 +3,28 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// variabili globali
+const playButton = document.getElementById("play");
+let randomNumbUser, randomNumbPc;
+
+// evento al button
+playButton.addEventListener("click", myFunction)
+
+// funzione al click
+function myFunction () {
+
+    // generazione numero user
+    randomNumbUser = Math.floor (Math.random() * 6) + 1;
+    console.log(randomNumbUser);
+
+    // generazione numero pc
+    randomNumbPc = Math.floor (Math.random() * 6) + 1;
+    console.log(randomNumbPc);
+
+    // numero uscito
+    document.getElementById("NumUser").innerHTML= randomNumbUser
+    document.getElementById("NumPc").innerHTML= randomNumbPc
+
+    // condizioni
+}
