f896a9516fd32561aee264fc8d87dee6381a84a8
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-04 14:56:24 +0200

    add conditions

--- Mail/js/myscript.js
+++ Mail/js/myscript.js
@@ -7,6 +7,7 @@
 // variabili globali
 const mailConsentite = ["saraz@gmail.com", "franc22@gmail.com", "al-berto@gmail.com", "jhonny@gmail.com"];
 const buttonCheck = document.getElementById("check");
+let esito, messaggioOutput;
 
 // evento al button
 buttonCheck.addEventListener('click', checkFunction)
@@ -14,4 +15,32 @@ buttonCheck.addEventListener('click', checkFunction)
 // funzione al check
 function checkFunction() {
     
+    // input utente
+    let userMail = document.getElementById("input_mail").value;
+    console.log(userMail);
+
+    // ciclo
+    for (i = 0; i < mailConsentite.length; i++)
+
+    // condizione
+    if (userMail == mailConsentite[i]) {
+        esito = "accetta"
+    }
+    
+    else {
+        esito = "rifiuta"
+    }
+
+
+    // output
+    if (esito == "accetta") {
+        messaggioOutput = "Accesso consentito."
+    }
+
+    else if ( esito == "rifiuta") {
+        messaggioOutput = "Mi dispiace, non puoi entrare."
+    }
+
+    console.log(messaggioOutput);
+
 }
